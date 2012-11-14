75aae684c0eb9d6d0396beece4ce9ec3a99257bf
halaszk <halaszk@deathlord.eu>
2012-11-14 11:52:45 +0100

    replace superuser to SuperSU in payload

--- compress-payload.sh
+++ compress-payload.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 cd PAYLOAD
-if [ -e Superuser.apk.xz ]; then
-	rm Superuser.apk.xz
+if [ -e SuperSU.apk.xz ]; then
+	rm SuperSU.apk.xz
 	rm su.xz
 fi;
 if [ -e payload.tar ]; then
 	rm -f payload.tar
 fi;
-chmod 644 Superuser.apk
+chmod 644 SuperSU.apk
 chmod 755 su
-xz -zekv9 Superuser.apk
+xz -zekv9 SuperSU.apk
 xz -zekv9 su
 
-mv Superuser.apk.xz res/misc/payload/
+mv SuperSU.apk.xz res/misc/payload/
 mv su.xz res/misc/payload/
 
 rm -f ../payload.tar
