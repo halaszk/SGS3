fe88fb0ceca6cfff48e0ddac25653b3a8e1f47f3
halaszk <halaszk@deathlord.eu>
2012-12-02 01:21:07 +0100

    update payload

--- compress-payload.sh
+++ compress-payload.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 cd PAYLOAD
-if [ -e Superuser.apk.xz ]; then
-	rm Superuser.apk.xz
-	rm su.xz
-fi;
 if [ -e payload.tar ]; then
 	rm -f payload.tar
 fi;
-chmod 644 Superuser.apk
-chmod 755 su
-xz -zekv9 Superuser.apk
-xz -zekv9 su
 
-mv Superuser.apk.xz res/misc/payload/
+mv SuperSU.apk.xz res/misc/payload/
+mv STweaks.apk.xz res/misc/payload/
 mv su.xz res/misc/payload/
 
 rm -f ../payload.tar
